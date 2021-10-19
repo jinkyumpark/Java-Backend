ed13e6b082a79a36e0be6313732187e0eb4639ca
Jinkyum Park <61900235+jinkyumpark@users.noreply.github.com>
2021-10-19 15:51:17 +0900

    Add files via upload

---
+++ Day 02/Println02.java
@@ -0,0 +1,15 @@
+package days02;
+
+public class Println02 {
+	public static void main(String[] args) {
+		// Printf03 에서 만들었던 성적표를 println 명령을 이용하여 출력
+		
+		System.out.println("\t\t###성적표###");
+		System.out.println("-------------------------------------------------");
+		System.out.println("번호\t성명\t\t국어\t영어\t수학\t총점\t평균");
+		
+		System.out.println("1\t박진겸\t\t96\t100\t100\t298\t99.3");
+		System.out.println("2\t손예림\t\t50\t50\t50\t150\t50");
+		System.out.println("3\t손로나\t\t0\t0\t0\t0\t0");
+	}
+}
