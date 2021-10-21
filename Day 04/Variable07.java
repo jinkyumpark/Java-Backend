cdb35b9bd6559132e3184930765d6ebd81220b10
Jinkyum Park <61900235+jinkyumpark@users.noreply.github.com>
2021-10-21 10:17:20 +0900

    Add files via upload

---
+++ Day 04/Variable07.java
@@ -0,0 +1,19 @@
+package days04;
+import java.util.Scanner;
+
+public class Variable07 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("Number of Apple: ");
+		int numApple = stdIn.nextInt();
+		
+		System.out.print("Number of Apple per Box: ");
+		int numPerBox = stdIn.nextInt();
+		
+		int numBoxPacked = numApple / numPerBox;
+		int numAppleLeft = numApple % numPerBox;
+		
+		System.out.printf("Number of Box Packed: %d\nNumber of Apples left: %d\n", numBoxPacked, numAppleLeft);
+		stdIn.close();
+	}
+}
